a2ea0272ed52a767e64b1c40962e9bcb509c798c
Jovana Ilin <45700907+jovanailin@users.noreply.github.com>
2024-12-11 15:45:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,22 +5,17 @@ from sqlalchemy.orm import sessionmaker
 from database.db_setup import DATABASE_URL
 
 def run_app():
-    """Run the Library Management System application."""
     root = tk.Tk()
     root.title("Library Management System")
 
-    # Create the SQLAlchemy engine and session
     engine = create_engine(DATABASE_URL)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Pass the session to MainGUI
     app = MainGUI(root, session)
 
-    # Start the Tkinter event loop
     root.mainloop()
 
-    # Close the session upon exiting
     session.close()
 
 if __name__ == "__main__":
